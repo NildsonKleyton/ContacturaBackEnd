23581cdb089ff9089a9c7e6f430d88bd3dddfb8e
Nildson <nildsonkleyton@hotmail.com>
2021-09-15 19:18:31 -0300

    projeto finalizando parte 1 add Cross

--- contactura/src/main/java/com/contactura/contactura/controller/ContacturaController.java
+++ contactura/src/main/java/com/contactura/contactura/controller/ContacturaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.contactura.contactura.model.Contactura;
 import com.contactura.contactura.repository.ContacturaRepository;
 
-
+@CrossOrigin()
 @RestController
 @RequestMapping({"/contactura"}) //http://localhost:8095/contactura
 public class ContacturaController {
